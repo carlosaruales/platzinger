729453ac531f2122f5ce0ac75d41a2210424b621
Andres ruales <carlosa.ruales@gmail.com>
2019-10-05 09:09:31 -0500

    Nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
