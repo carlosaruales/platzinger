21719b6db9eae3ac1b6f99103b1d414d4e063598
Andres ruales <carlosa.ruales@gmail.com>
2019-10-05 10:29:52 -0500

    recorriendo usuarios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,8 +8,10 @@ import { User } from 'src/app/interfaces/user';
 })
 export class HomeComponent implements OnInit {
 
+  friends:User[];
+
   constructor() { 
-    let myUser:User ={
+    let usuario1:User ={
       nick: "Carlos",
       age: 26,
       email: "carlosa.ruales@gmail.com",
@@ -17,11 +19,40 @@ export class HomeComponent implements OnInit {
       uid:1
     }
 
-    let users: User[] = [];
+    let usuario2:User ={
+      nick: "Marcela",
+      age: 26,
+      email: "marcela.ruales@gmail.com",
+      friend:true,
+      uid:2
+    }
 
-    users.push(myUser);
+    let usuario3:User ={
+      nick: "Deyanira",
+      age: 26,
+      email: "deyanira.acosta@gmail.com",
+      friend:true,
+      uid:3
+    }
 
-    console.log(myUser);
+    let usuario4:User ={
+      nick: "Johana",
+      age: 26,
+      email: "deyanira.acosta@gmail.com",
+      friend:true,
+      uid:4
+    }
+
+    let usuario5:User ={
+      nick: "Alberto",
+      age: 26,
+      email: "alberto.ruales@gmail.com",
+      friend:true,
+      uid:5
+    }
+
+    this.friends = [usuario1,usuario2,usuario3,usuario4,usuario5];
+
   }
 
   ngOnInit() {
