10972138b12591d5a64fa3903feb905e61a11dde
Andres ruales <carlosa.ruales@gmail.com>
2019-10-06 08:34:11 -0500

    se qeudo por fuera del commit

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  friends:User[];
+  constructor() {
+    let usuario1:User ={
+      nick: "Carlos",
+      age: 26,
+      email: "carlosa.ruales@gmail.com",
+      friend:true,
+      uid:1
+    }
+
+    let usuario2:User ={
+      nick: "Marcela",
+      age: 26,
+      email: "marcela.ruales@gmail.com",
+      friend:false,
+      uid:2
+    }
+
+    let usuario3:User ={
+      nick: "Deyanira",
+      age: 26,
+      email: "deyanira.acosta@gmail.com",
+      friend:true,
+      uid:3
+    }
+
+    let usuario4:User ={
+      nick: "Johana",
+      age: 26,
+      email: "deyanira.acosta@gmail.com",
+      friend:true,
+      uid:4
+    }
+
+    let usuario5:User ={
+      nick: "Alberto",
+      age: 26,
+      email: "alberto.ruales@gmail.com",
+      friend:true,
+      uid:5
+    }
+
+    this.friends = [usuario1,usuario2,usuario3,usuario4,usuario5];
+  }
+
+  public getFriends(){
+    return this.friends;
+  }
+}
